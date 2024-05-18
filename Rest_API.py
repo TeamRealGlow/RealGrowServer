9b9aac7444420f3ca2d64b8a6b376bf41220e721
00seungho <tmdgh9910@naver.com>
2024-05-18 17:42:41 +0900

    Load Item actualize

--- Rest_API.py
+++ Rest_API.py
@@ -1,9 +1,9 @@
 import os.path
-
+import json
 from flask import Flask,request
 from flask_restx import Api, Resource
 from face_parsing.face_parser import Parser
-
+from flask import abort
 import base64
 from io import BytesIO
 from PIL import Image
@@ -13,18 +13,38 @@ api = Api(app)
 
 
 @api.route('/parsing')
-class HelloWorld(Resource):
+class Parsing(Resource):
     def post(self):
-        json_data = request.get_json()
-        img = json_data['img']
-        img = base64.b64decode(img)
-        img = Image.open(BytesIO(img))
-        parsing = parser.out_parsing(img)
-        byte_img = BytesIO()
-        parsing.save(byte_img,format="PNG")
-        parsing = byte_img.getvalue()
-        b64_string = base64.b64encode(parsing).decode('utf-8')
-        return {"PNGImage": b64_string}
+        try:
+            json_data = request.get_json()
+            img = json_data['img']
+            img = base64.b64decode(img)
+            img = Image.open(BytesIO(img))
+            parsing = parser.out_parsing(img)
+            byte_img = BytesIO()
+            parsing.save(byte_img,format="PNG")
+            parsing = byte_img.getvalue()
+            b64_string = base64.b64encode(parsing).decode('utf-8')
+            return {"PNGImage": b64_string}
+        except Exception as e:
+            print(e)
+            abort(400,f"{e}")
+
+
+
+@api.route("/loadItem/<string:Item>")
+class LoadItem(Resource):
+    def get(self,Item):
+        rootpath =os.path.join("static","ItemDir")
+        Jsonpath =os.path.join(rootpath,Item+".json")
+        try:
+            with open(Jsonpath,'r',encoding="utf-8") as file:
+                json_data = json.load(file)
+                body = {"Category": Item,"Itemlen": len(json_data["row"]),"row":json_data["row"]}
+                print(body)
+                return body
+        except FileNotFoundError:
+            abort(400,f"{Item} is not found")
 
 if __name__ == '__main__':
     # debug를 True로 세팅하면, 해당 서버 세팅 후에 코드가 바뀌어도 문제없이 실행됨.
--- client.py
+++ client.py
@@ -1,30 +1,47 @@
-import numpy as np
-import requests
-import base64
-import cv2
-import os
-from PIL import Image
-import matplotlib.pyplot as plt
-from io import BytesIO
+def imgRequestTest():
+    import numpy as np
+    import requests
+    import base64
+    import cv2
+    import os
+    from PIL import Image
+    import matplotlib.pyplot as plt
+    from io import BytesIO
 
-image_name = os.path.join("ex","oh.png")
-with open(image_name, 'rb') as f:
-    img_data = f.read()
+    image_name = os.path.join("ex","oh.png")
+    with open(image_name, 'rb') as f:
+        img_data = f.read()
 
-b64_string = base64.b64encode(img_data).decode('utf-8')
+    b64_string = base64.b64encode(img_data).decode('utf-8')
 
-files = {
-            "img": b64_string,
-        }
+    files = {
+                "img": b64_string,
+            }
 
-r = requests.post("http://192.168.24.185:8000/parsing", json=files)
+    r = requests.post("http://192.168.24.185:8000/parsing", json=files)
 
-print(r)
-img = r.json()["PNGImage"]
-img = base64.b64decode(img)
-img = BytesIO(img)
-img = Image.open(img)
-plt.imshow(img)
-plt.show()
-np_img = np.array(img)
-print(np_img.shape)
+    print(r)
+    img = r.json()["PNGImage"]
+    img = base64.b64decode(img)
+    img = BytesIO(img)
+    img = Image.open(img)
+    plt.imshow(img)
+    plt.show()
+    np_img = np.array(img)
+    print(np_img.shape)
+
+def ItemGetTest():
+    import requests
+    import base64
+    import os
+    r = requests.get("http://192.168.24.185:8000/loadItem/hairItem1")
+    b = r.json()
+    print(b)
+
+
+if __name__ == "__main__":
+    choice = 2
+    if choice == 1:
+        imgRequestTest()
+    elif choice == 2:
+        ItemGetTest()
